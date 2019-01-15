5efb6a281d536a8da1f25df2efc231563d4ccdbb
lillofm <lillofm@gmail.com>
2019-01-15 22:36:48 +0100

    carreracon Pygame

--- turtleRaceLunesII/main.py
+++ turtleRaceLunesII/main.py
@@ -1,15 +1,40 @@
 import pygame, sys
+import random
 
+
+class Runner():
+    __customes = ('turtle', 'fish', 'prawn', 'moray', 'octopus')
+    
+    
+    def __init__(self, x=0, y=0):
+        ixCustome = random.randint(0, 4)
+        
+        self.custome = pygame.image.load("image/{}.png".format(self.__customes[ixCustome]))
+        self.position = [x, y]
+        self.name = ""
+        
+    def avanzar(self):
+        self.position[0] += random.randint(1, 6)
+    
 
 class Game():
     runners = []
-    __starLine = 20
+    __posY = (160, 200, 240, 280)
+    __names = ("Speedy", "Lucera", "Alonso", "Torcuata")
+    __startLine = -5
     __finishLine = 620
     
     def __init__(self):
         self.__screen = pygame.display.set_mode((640, 480))
-        self.__background = pygame.image.load("image/background.png")#insertamos esta imagen como fondo de pantalla. pero no la hemos puesto en pantalla. Solo la hemmos puesto en una variable
-        pygame.display.set_caption("carrera de bichos")#le poenmos este nombre
+        self.__background = pygame.image.load("image/background.png")
+        pygame.display.set_caption("Carrera de bichos")
+        
+        for i in range (4):
+            theRunner = Runner(self.__startLine, self.__posY[i])
+            theRunner.name = self.__names[i]
+            self.runners.append(theRunner)
+            
+
         
     def competir(self):
         gameOver = False
@@ -17,16 +42,28 @@ class Game():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     gameOver = True
-                    
-            self.__screen.blit(self.__background, (0, 0))#Me pones en la pantalla, lo que hay en la variable backgruond(la imagen) en la posicion original (0, 0)
+
+            for activeRunner in self.runners:
+                activeRunner.avanzar()
+                if activeRunner.position[0] >= self.__finishLine:
+                    print("{} ha ganado".format(activeRunner.name))
+                    gameOver = True
             
-            pygame.display.flip()#refrescar la pantalla
+            self.__screen.blit(self.__background, (0, 0))
             
-        pygame.quit()#cierra pygame. sie el juego es muy complejo, es posible que tarde en cerrase o incluso se quede colgado mientras intenta cerrar
-        sys.exit()#cierra python y nos aseguramos de cerrarlo
-    
-    
-if __name__ == "__main__":
+            for runner in self.runners:
+                self.__screen.blit(runner.custome, runner.position)                
+            
+            pygame.display.flip()
+        
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+        
+if __name__ == '__main__':
     game = Game()
-    pygame.init()#si no unciona poner pygame.font.init()
+    pygame.font.init()
     game.competir()
